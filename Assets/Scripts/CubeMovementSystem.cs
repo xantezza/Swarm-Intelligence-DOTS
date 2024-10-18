92387afb3220fc1f92e4ab1f6b46793184517fae
xantezza <itori.ohm@gmail.com>
2024-10-18 22:13:53 +0300

    fix bug

--- Assets/Scripts/CubeMovementSystem.cs
+++ Assets/Scripts/CubeMovementSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
@@ -6,10 +7,8 @@ using Unity.Transforms;
 
 namespace DefaultNamespace
 {
-    [BurstCompile]
     public partial struct CubeMovementSystem : ISystem
     {
-        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var entityManager = state.EntityManager;
@@ -20,20 +19,29 @@ namespace DefaultNamespace
                 if (entityManager.HasComponent<CubeComponent>(entity))
                 {
                     var cube = entityManager.GetComponentData<CubeComponent>(entity);
-                    var localTransform = entityManager.GetComponentData<LocalTransform>(entity);
-                    float3 moveDirection = cube.moveDirection * SystemAPI.Time.DeltaTime * cube.moveSpeed;
-                    localTransform.Position += moveDirection;
-                    entityManager.SetComponentData(entity, localTransform);
 
+                    cube.moveSpeed -= 1 * SystemAPI.Time.DeltaTime;
                     if (cube.moveSpeed > 0)
                     {
-                        cube.moveSpeed -= 1 * SystemAPI.Time.DeltaTime;
                         entityManager.SetComponentData(entity, cube);
+                        var localTransform = entityManager.GetComponentData<LocalTransform>(entity);
+                        float3 moveDirection = cube.moveDirection * SystemAPI.Time.DeltaTime * cube.moveSpeed;
+                        var target = quaternion.LookRotationSafe(moveDirection, math.up());
+                        localTransform.Rotation = target;
+                        localTransform = localTransform.RotateZ(-(float) SystemAPI.Time.ElapsedTime * 10);
+                        localTransform.Position += moveDirection;
+                        entityManager.SetComponentData(entity, localTransform);
                     }
-                    else
+                    else 
                     {
-                        cube.moveSpeed = 0;
                         entityManager.AddComponentData(entity, new HDRPMaterialPropertyBaseColor {Value = new float4(1, 0, 0, 1)});
+                        var buffer = entityManager.GetBuffer<Child>(entity).ToNativeArray(Allocator.Temp);
+
+                        foreach (var child in buffer)
+                        {
+                            entityManager.AddComponentData(child.Value, new HDRPMaterialPropertyBaseColor {Value = new float4(1, 0, 0, 1)});
+                        }
+
                         entityManager.RemoveComponent<CubeComponent>(entity);
                     }
                 }
